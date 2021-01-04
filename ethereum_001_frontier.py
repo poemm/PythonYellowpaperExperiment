c0d3301c9f456b8ec17ad76d1008fc595d606039
poemm <36397285+poemm@users.noreply.github.com>
2021-01-04 14:15:00 -0600

    section 2

---
+++ ethereum_001_frontier.py
@@ -0,0 +1,38 @@
+import json
+import math
+import crypto
+
+
+verbose = 0
+debug = 0
+
+
+# This file implements some sections of the Ethereum yellowpaper.
+
+
+
+##############################
+# 2. The Blockchain Paradigm #
+##############################
+
+# sigma is the state, T is a transaction
+
+# the Ethereum state transition function (STF)
+#def Upsilon(sigma,T):
+#  # defined in section 6.2
+#   pass
+
+# B is a block, which includes a list of transactions, see section 4.3
+
+# the block-level state-transition function (called "state-accumulation function" in section 4.3.2)
+#def Pi(sigma,B):
+#  # defined in section 11.4
+#  pass
+
+
+# the block-finalization state transition function; rewards the miner
+#def Omega(sigma,B):
+#  # defined in section 9
+#  pass
+
+
