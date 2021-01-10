b93ef46af0e5b346e54874fa7bbfef416243fe05
poemm <36397285+poemm@users.noreply.github.com>
2021-01-10 11:45:20 -0600

    section 3

--- ethereum_001_frontier.py
+++ ethereum_001_frontier.py
@@ -36,3 +36,57 @@ debug = 0
 #  pass
 
 
+
+##################
+# 3. Conventions #
+##################
+
+# lowercase Greek letters are state values
+# sigma is the world state
+# mu is the machine state
+
+# uppercase Greek letters are functions which operate on the state
+# e.g. Upsilon is the Ethereum STF
+
+# keccak-256
+def KEC(bytes_):
+  return crypto.keccak256(bytes_)
+
+# keccak-512
+def KEC512(bytes_):
+  return crypto.keccak512(bytes_)
+
+# uppercase letters are functions or tuples
+# e.g. C is the cost function
+# e.g. T is a transaction
+# can be subscripted
+#   with uppercase subscript is component of tuple e.g. C_SSTORE
+#   with component e.g. T.n is nonce of tx
+
+# lowercase letters denote scalars and byte arrays (bold in yellowpaper)
+# e.g. n is nonce
+# e.g. greek delta is number of items required on stack for a give operation
+# e.g. o is byte array output by a message call
+
+# bold uppercase can be used for important values
+
+# $\mathbb{N}$ is non-negative integers
+# $\mathbb{B}$ is all byte sequences, see appx B
+# $\mathbb{B}_{32}$ is all byte sequences of length 32
+# $\mathbb{N}_{256}$ is non-negative integers less than 2^256, see section 4.3
+
+# square brackets index components or subsequences of a sequence
+# e.g. mu.s[0] is top of stack
+# ellipses specify a range
+# e.g. mu_m[0..31] is first 32 items of memory
+
+# state sigma is sequence of accounts, use square brackets to access account by address
+
+# if value takes intermediate values, first value is <blah>, modified value is <blah>', intermediate values are <blah>*, <blah>**, ..., <blah>'
+# may also use alpha-numeric subscripts to denote intermediate values
+
+# given function f, f* is f over a sequence, element-wise, see section 4.3
+
+# last item of a sequence
+def l(x):
+  return x[len(x)-1]
