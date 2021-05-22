50e2a9a4081d652c07d6f4bc8ce1e23c8b973a89
poemm <36397285+poemm@users.noreply.github.com>
2021-05-22 15:31:59 -0500

    appendix b rlp inverses

--- ethereum_001_frontier.py
+++ ethereum_001_frontier.py
@@ -1446,3 +1446,84 @@ def s(x):
   return sx
 
 
+# inverses of above
+
+def RLP_inv(b):
+  if verbose: print("RLP_inv(",b.hex(),")")
+  if len(b)==0:
+    return bytearray([0x80])
+  if b[0]<0xc0 : # bytes
+    return R_b_inv(b)
+  else:
+    return R_l_inv(b)
+
+def R_b_inv(b):
+  if verbose: print("R_b_inv(",b.hex(),")")
+  if len(b)==1 and b[0]<0x80:
+    return b #bytearray([b[0]-0x80])
+  elif b[0]<=0xb7:
+    return b[1:1+b[0]-0x80]
+  else:
+    len_BElenx = b[0] - 183
+    lenx = BE_inv(b[1:len_BElenx+1]) #TODO lenx unused
+    return b[len_BElenx+1:len_BElenx+1+lenx]
+
+def BE_inv(b):
+  if verbose: print("BE_inv(",b.hex(),")")
+  x=0
+  for n in range(len(b)):
+    #x+=b[n]*2**(len(b)-1-n)
+    x+=b[n]*2**(8*(len(b)-1-n))
+  return x
+
+def R_l_inv(b):
+  if verbose: print("R_l_inv(",b.hex(),")")
+  if b[0] <= 0xf7:
+    lensx = b[0]-0xc0
+    sx = b[1:1+lensx]
+  else:
+    len_lensx = b[0] - 247
+    lensx = BE_inv(b[1:1+len_lensx])
+    sx = b[1+len_lensx : 1+len_lensx+lensx]
+  return s_inv(sx)
+
+def s_inv(b):
+  if verbose: print("s_inv(",b.hex(),")")
+  x=[]
+  i=0
+  len_=len(b)
+  while i<len_:
+    len_cur, len_len_cur = decode_length(b[i:])
+    x += [RLP_inv(b[i:i+len_len_cur+len_cur])]
+    i += len_cur + len_len_cur
+    if debug: print("  s_inv() returning",x)
+  if debug: print("  s_inv() returning",x)
+  return x
+
+# this is a helper function not described in the spec
+# but the spec does not discuss the inverse to he RLP function, so never has the opportunity to discuss this
+# returns the length of an encoded rlp object
+def decode_length(b):
+  if verbose: print("length_inv(",b.hex(),")")
+  if len(b)==0:
+    return 0,0 # TODO: this may be an error
+  length_length=0
+  first_rlp_byte = b[0]
+  if first_rlp_byte < 0x80:
+    rlp_length=1
+    return rlp_length, length_length
+  elif first_rlp_byte <= 0xb7:
+    rlp_length = first_rlp_byte - 0x80
+  elif first_rlp_byte <= 0xbf:
+    length_length = first_rlp_byte - 0xb7
+    rlp_length = BE_inv(b[1:1+length_length])
+  elif first_rlp_byte <= 0xf7:
+    rlp_length = first_rlp_byte - 0xc0
+  elif first_rlp_byte <= 0xbf:
+    length_length = first_rlp_byte - 0xb7
+    rlp_length = BE_inv(b[1:1+length_length])
+  return rlp_length, 1+length_length
+
+
+
+
